e20fb1bb1a56c081485b0c83bb5d07b9cd689f4e
Rupert Rutledge <1982481+Eosis@users.noreply.github.com>
2021-12-11 10:35:02 +0000

    22 stars

---
+++ src/day_11/mod.rs
@@ -0,0 +1,143 @@
+use hashbrown::HashSet;
+use std::fs;
+use std::path::Path;
+
+type Input = Vec<Vec<usize>>;
+pub fn solve_part_1() -> Result<(), ()> {
+    let mut input = parse_from_file("./inputs/day11.txt");
+    println!("Solution: {}", part_one(&mut input));
+    Ok(())
+}
+
+pub fn solve_part_2() -> Result<(), ()> {
+    let mut input = parse_from_file("./inputs/day11.txt");
+    println!("Solution: {}", part_two(&mut input));
+    Ok(())
+}
+
+fn parse_from_file<T: AsRef<Path>>(filename: T) -> Input {
+    let input = fs::read_to_string(filename).unwrap();
+    parse_from_str(&input)
+}
+
+fn parse_from_str(input: &str) -> Input {
+    input
+        .lines()
+        .map(|line| line.chars().map(|n| n.to_digit(10).unwrap() as usize).collect())
+        .collect()
+}
+
+pub fn part_one(mut grid: &mut [Vec<usize>]) -> usize {
+    let mut flashes = 0;
+    for step in 0..100 {
+        flashes += iterate_dem_octos(&mut grid);
+        print_grid(& grid);
+    }
+    flashes
+}
+
+fn iterate_dem_octos(grid: &mut [Vec<usize>]) -> usize {
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            grid[y][x] += 1;
+        }
+    }
+
+    let mut flashed: HashSet<(usize, usize)> = HashSet::new();
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            if grid[y][x] >= 10 {
+                flash_dem_neighbours(grid, (y, x), &mut flashed);
+            }
+        }
+    }
+
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            if grid[y][x] >= 10 {
+                grid[y][x] = 0;
+            }
+        }
+    }
+    flashed.len()
+}
+
+fn print_grid(grid: &[Vec<usize>]) {
+    for row in grid {
+        for val in row {
+            print!(" {:2} ", val);
+        }
+        println!();
+    }
+}
+
+fn flash_dem_neighbours(grid: &mut [Vec<usize>], (y, x): (usize, usize), flashed: &mut HashSet<(usize,usize)>) {
+    if flashed.contains(&(y, x)) {
+        return;
+    }
+    flashed.insert((y, x));
+    let neighbours = get_neighbours(grid, (y, x));
+    for (other_y, other_x) in neighbours {
+        grid[other_y][other_x] += 1;
+        if grid[other_y][other_x] >= 10 {
+            flash_dem_neighbours(grid, (other_y, other_x), flashed);
+        }
+    }
+}
+
+fn get_neighbours(grid: &[Vec<usize>], (y, x): (usize, usize)) -> impl Iterator<Item=(usize, usize)> {
+    let (y, x) = (y as i32, x as i32);
+    let max_x = grid[0].len() as i32;
+    let max_y = grid.len() as i32;
+    (-1..=1).flat_map(move |dy| {
+        (-1..=1).map(move | dx | {
+            if dx == 0 && dy == 0 {
+                None
+            } else if dx + x < 0 || dx + x >= max_x {
+                None
+            } else if dy + y < 0 || dy + y >= max_y {
+                None
+            } else {
+                Some(((y + dy) as usize, (x + dx) as usize))
+            }
+        })
+    }).flatten()
+}
+
+pub fn part_two(mut grid: &mut [Vec<usize>]) -> usize {
+    let mut step = 0;
+    let size = grid[0].len() * grid.len();
+    loop {
+        step += 1;
+        let flashes_this_time = iterate_dem_octos(&mut grid);
+        if flashes_this_time == size {
+            break step;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = include_str!("../../test_inputs/day11.txt");
+
+    #[test]
+    fn test_part_one() {
+        let mut input = parse_from_str(TEST_INPUT);
+        assert_eq!(part_one(&mut input), 1656);
+    }
+
+    #[test]
+    fn test_get_neighbours() {
+        let mut input =  parse_from_str(TEST_INPUT);
+        dbg!(get_neighbours(&mut input, (1, 1)).collect::<Vec<_>>());
+        assert_eq!(get_neighbours(&mut input, (2, 2)).count(), 8)
+    }
+
+    #[test]
+    fn test_part_two() {
+        let mut input = parse_from_str(TEST_INPUT);
+        assert_eq!(part_two(&mut input), 195);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod day_7;
 mod day_8;
 mod day_9;
 mod day_10;
+mod day_11;
 
 use argh::FromArgs;
 
@@ -49,6 +50,8 @@ fn main() -> Result<()> {
         (9, 2) => day_9::solve_part_2().expect(FAILURE_TEXT),
         (10, 1) => day_10::solve_part_1().expect(FAILURE_TEXT),
         (10, 2) => day_10::solve_part_2().expect(FAILURE_TEXT),
+        (11, 1) => day_11::solve_part_1().expect(FAILURE_TEXT),
+        (11, 2) => day_11::solve_part_2().expect(FAILURE_TEXT),
         (_, _) => unimplemented!("This day no work yet, brah."),
     };
     Ok(())
